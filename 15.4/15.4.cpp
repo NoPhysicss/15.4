61b228133325395329606f45ac7d329790437c87
alex <a>
2024-03-31 17:34:55 +0400

    Updatet

--- 15.4/15.4.cpp
+++ 15.4/15.4.cpp
@@ -1,40 +1,52 @@
 ﻿#include <iostream>
 
+void odd(const int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if ((i % 2) == 1)
+        {
+            std::cout << i << " | ";
+        }
+    }
+}
+void writh(const int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if ((i % 2) == 0)
+        {
+            std::cout << i << " | ";
+        }
+    }
+}
+
 int main()
 {
-    int a;
+    int a = 0;
     std::cout << "enter number from 0 to N\n";
     std::cout << "N = ";
     std::cin >> a;
 
-    const int N = a;
 
     int b = 0;
 
     std::cout << "for even numbers write 2, and for odd numbers write 1: ";
     std::cin >> b;
 
-    for (int i = 1; i <= N; i++)
-    {
+
         if (b == 2)
         {
-            if ((i % 2) == 0)
-            {
-                std::cout << i << '\n';
-            }
+            writh(a);
         }
         else if (b == 1)
         {
-            if ((i % 2) == 1)
-            {
-                std::cout << i << '\n';
-            }
+            odd(a);
         }
         else if ((b != 2) && (b != 1))
         {
             std::cout << "realy?";
             return 1;
         }
-    }
     return 0;
 }
